87f8b2d2b0ee9485213ecaeb8b08a9437c91dfbe
Ramil <mukhametzyanov@it-projects.info>
2019-08-24 16:45:29 +0500

    +Timer

--- dict.js
+++ dict.js
@@ -1,37 +1,40 @@
 Text = "";
-Alphabet = {count: 0, chars: [], sort: []};
+Alphabet = {count: 0, time: 0, chars: [], sort: []};
 Alphabet.new_e = function (e){
- len = this.count++;
+ this.count++;
+ len = this.chars.length;
  ar = this.chars;
  ar[len] = new Object();
- ar[len].l = e;
- ar[len].c = 1;
+ ar[len].symbol = e; //symbol
+ ar[len].counter = 1; //counter
+ ar[len].time = 0; //time
  this.sort[len] = len;
 }
 Alphabet.add_e = function (e, ind){
+ this.count++;
  v = this.chars[ind];
- if(v.l != e) return -1;
- v.c++;
+ if(v.symbol != e) return -1;
+ v.counter++;
  return 0; 
 }
 Alphabet.search_e = function (e){
  m = -1;
- for(var i = 0; i < this.count; i++){
-  if(this.chars[this.sort[i]].l == e){
+ for(var i = 0; i < this.chars.length; i++){
+  if(this.chars[this.sort[i]].symbol == e){
    m = i; break;
   }
  }
  return m;
 }
 Alphabet.sorting = function (m){
- if(m >= this.count) return;
+ if(m >= this.chars.length) return;
  if(m <= 0) return
  ind = this.sort[m];
  ar = this.chars;
- n = ar[ind].c;
+ n = ar[ind].counter;
  var tmp = -1;
  for(var i = m; i >= 1; i--){
-  if( ar[this.sort[i-1]].c < n){
+  if( ar[this.sort[i-1]].counter < n){
    this.sort[i] = this.sort[i-1];
    this.sort[i-1] = ind;
   }
@@ -42,7 +45,7 @@ Alphabet.add = function (e){
  m = this.search_e(e);
  if(m == -1){
   this.new_e(e);
-  this.sorting(this.count-1);
+  this.sorting(this.chars.length-1);
  }else{
   this.add_e(e, this.sort[m]);
   this.sorting(m);
@@ -50,8 +53,8 @@ Alphabet.add = function (e){
 }
 Alphabet.show_sorted = function(){
 var sar = [];
- for(i = 0 ; i < this.count; i++){
- sar[i] = this.chars[this.sort[i]].l;
+ for(i = 0 ; i < this.chars.length; i++){
+ sar[i] = this.chars[this.sort[i]].symbol;
 }
  return sar;
 }
@@ -60,5 +63,35 @@ Text = document.getElementById(id).value;
 for (var i = 0; i < Text.length; i++){
  Alphabet.add(Text.charAt(i));
 }
-
 }
+
+Timer = {begin: 0, current: 0, time: 0, end: 0};
+Timer.start = function (){
+ this.begin = new Date();
+}
+Timer.init = function(begin,current,time,end,stop){
+this._begin=begin;
+this._current=current;
+this._time=time;
+this._end=end;
+this._stop=stop;
+}
+Timer.show = function(){
+ document.getElementById(this._begin).innerHTML = this.begin;
+ this.current = new Date();
+ document.getElementById(this._current).innerHTML = this.current;
+ this.time = this.current - this.begin;
+ document.getElementById(this._time).innerHTML = this.time;
+ document.getElementById(this._end).innerHTML = this.end;
+}
+Timer.show_stop = function(){
+ document.getElementById(_stop).innerHTML = "Stop";
+}
+Timer.hide = function(begin,current,time,end){
+ document.getElementById(_begin).innerHTML = "";
+ document.getElementById(_current).innerHTML = "";
+ document.getElementById(_time).innerHTML = "";
+ document.getElementById(_end).innerHTML = "";
+ document.getElementById(_stop).innerHTML = "";
+}
+Timer.init("begin","current","time","end","stop");
